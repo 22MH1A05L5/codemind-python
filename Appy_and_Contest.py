528c8bd8fdbbb588b86478fc45ea96b2f0ce06e8
sudhir yadav <119724200+22MH1A05L5@users.noreply.github.com>
2023-06-27 11:07:28 +0530

    from math import gcd def lcm(a, b):     return a * b // gcd(a, b) t = int(input()) for _ in range(t):     n, a, b, k = map(int, input().split())     x = (n // a) - (n // lcm(a, b))     y = (n // b) - (n // lcm(a, b))     z = (n // lcm(a, b)) - (x + y)     if x + y >= k:         print("Win")     else:      print("Lose")

--- Appy_and_Contest.py
+++ Appy_and_Contest.py
@@ -1,9 +1,7 @@
 from math import gcd
 def lcm(a, b):
     return a * b // gcd(a, b)
-
 t = int(input())
-
 for _ in range(t):
     n, a, b, k = map(int, input().split())
     x = (n // a) - (n // lcm(a, b))
